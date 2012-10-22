0efbc31a35c44e17b8f8a5a627273f24328a6d2a
Brilly Tsang <brilly.tsang@gmail.com>
2012-10-22 11:15:59 +0800

    Just testing checkin for a new file to ensure the github integration in Intellij is ok

---
+++ src/main/java/akka/tutorial/first/java/TestingChecking.java
@@ -0,0 +1,15 @@
+package akka.tutorial.first.java;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Brilly
+ * Date: 10/22/12
+ * Time: 11:14 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class TestingChecking {
+
+    public static void main(String[] args) {
+        System.out.println("Hello from Brilly");
+    }
+}
