b9536380af2160b279efec0497937313cbf5b2ac
Brilly Tsang <brilly.tsang@gmail.com>
2012-11-29 16:48:45 +0800

    Completed Fork Join Pool Model

---
+++ src/main/java/com/ubs/gedit/gedex/actor/ExecutorExample.java
@@ -0,0 +1,60 @@
+package com.ubs.gedit.gedex.actor;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+public class ExecutorExample {
+
+    public static final int NTHREDS = 4;
+
+    private double currentPrice;
+    private double strike;
+    private double riskFreeRate;
+    private double tau;
+    private double annualVolatility;
+
+    public ExecutorExample(double currentPrice, double strike, double riskFreeRateInPercent, double tau, double annualVolatilityInPercent) {
+        this.currentPrice = currentPrice;
+        this.strike = strike;
+        this.riskFreeRate = riskFreeRateInPercent / 100;
+        this.tau = tau;
+        this.annualVolatility = annualVolatilityInPercent / 100;
+    }
+
+    public double digitalOption(double payoff, int timePeriods, int simulation) throws Exception {
+
+
+
+        ExecutorService executor = Executors.newFixedThreadPool(NTHREDS);
+        List<Future<Double>> priceResults = new ArrayList<Future<Double>>();
+        for (int i = 0; i < simulation; i++) {
+            Callable<Double> worker = new FinalPriceCallableSimulation(tau, riskFreeRate, annualVolatility, currentPrice, timePeriods);
+            Future<Double> submit = executor.submit(worker);
+            priceResults.add(submit);
+        }
+        System.out.println("Number of price simulated = " + priceResults.size());
+
+        double totalResult = 0;
+        // Now retrieve the result
+        for (Future<Double> price : priceResults) {
+            double result = price.get() >= strike ? payoff : 0;
+            totalResult += result;
+        }
+        executor.shutdown();
+
+        double average = totalResult / simulation;
+        double pvAverage = average * Math.exp(-riskFreeRate*tau);
+//        System.out.println("Average = " + average);
+//        System.out.println("pvAverage = " + pvAverage);
+
+        return pvAverage;
+    }
+
+    public double blackScholePrice() {
+        return 0;
+    }
+}
---
+++ src/main/java/com/ubs/gedit/gedex/actor/TestDriver.java
@@ -0,0 +1,52 @@
+package com.ubs.gedit.gedex.actor;
+
+public class TestDriver {
+
+    public static final int NUMBER_OF_SIMULATION = 1000000;
+    public static final int TIME_STEPS = 30;
+
+    public static void main(String[] args) throws Exception {
+
+        SingleThreadExample singleThread = new SingleThreadExample(36, 40, 4, 1, 24);
+        System.out.println("Black Scholes result = " + singleThread.blackScholePrice());
+
+        long startTime = System.currentTimeMillis();
+        double result = singleThread.digitalOption(1, TIME_STEPS, NUMBER_OF_SIMULATION);  //4000 millisecs for 1,000,000
+//        double result = singleThread.digitalOption(1, 30, 10000000);  //37 secs for 10,000,000
+        double duration = (System.currentTimeMillis() - startTime);
+        System.out.println("Digital Option with payoff $1, simulated return = " + result);
+        System.out.println("(" + duration + " millisec); Java version = " + System.getProperty("java.version"));
+
+        System.out.println("\n\nExecutor Model - Java 1.5-1.6");
+        ExecutorExample executor = new ExecutorExample(36, 40, 4, 1, 24);
+
+        startTime = System.currentTimeMillis();
+        result = executor.digitalOption(1, TIME_STEPS, NUMBER_OF_SIMULATION);  //4000 millisecs for 1,000,000
+//        result = executor.digitalOption(1, 30, 10000000);  //37 secs for 10,000,000
+        duration = (System.currentTimeMillis() - startTime);
+        System.out.println("Digital Option with payoff $1, simulated return = " + result);
+        System.out.println("(" + duration + " millisec); Executor Model");
+
+        System.out.println("\n\nNaive Fork Join Pool - Java 1.7");
+        NaiveForkJoinPoolExample naivePool = new NaiveForkJoinPoolExample(36, 40, 4, 1, 24);
+
+        startTime = System.currentTimeMillis();
+        result = naivePool.digitalOption(1, TIME_STEPS, NUMBER_OF_SIMULATION);  //4000 millisecs for 1,000,000
+//        result = executor.digitalOption(1, 30, 10000000);  //37 secs for 10,000,000
+        duration = (System.currentTimeMillis() - startTime);
+        System.out.println("Digital Option with payoff $1, simulated return = " + result);
+        System.out.println("(" + duration + " millisec); Naive Fork Join Model");
+
+
+        System.out.println("\n\nFork Join Pool - Java 1.7");
+        ForkJoinPoolExample pool = new ForkJoinPoolExample(36, 40, 4, 1, 24);
+
+        startTime = System.currentTimeMillis();
+        result = pool.digitalOption(1, TIME_STEPS, NUMBER_OF_SIMULATION);  //4000 millisecs for 1,000,000
+//        result = executor.digitalOption(1, 30, 10000000);  //37 secs for 10,000,000
+        duration = (System.currentTimeMillis() - startTime);
+        System.out.println("Digital Option with payoff $1, simulated return = " + result);
+        System.out.println("(" + duration + " millisec); Fork Join Model");
+    }
+
+}
